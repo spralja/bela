f451667440959ff2f0c6438b2d78b953480f06b6
spralja <spralja@ruc.dk>
2021-02-25 06:46:52 +0100

    Updated Rank to fit testing requirements

--- src/bela/Rank.java
+++ src/bela/Rank.java
@@ -11,7 +11,56 @@ public enum Rank {
     A,
     ;
 
-    public String toString(Suit trumpSuit) {
+    public int value(boolean isTrumpSuit) {
+        if(isTrumpSuit) {
+            return switch(this) {
+                case VII, VIII -> 0;
+                case IX -> 14;
+                case X -> 10;
+                case J -> 20;
+                case Q -> 3;
+                case K -> 4;
+                case A -> 11;
+            };
+        } else {
+            return switch(this) {
+                case VII, VIII, IX -> 0;
+                case X -> 10;
+                case J -> 2;
+                case Q -> 3;
+                case K -> 4;
+                case A -> 11;
+            };
+        }
+    }
+
+    public int power(boolean isTrumpSuit) {
+        if(isTrumpSuit) {
+            return switch(this) {
+                case VII -> 8;
+                case VIII -> 9;
+                case IX -> 14;
+                case X -> 12;
+                case J -> 15;
+                case Q -> 10;
+                case K -> 11;
+                case A -> 13;
+            };
+        } else {
+            return switch(this) {
+                case VII -> 0;
+                case VIII -> 1;
+                case IX -> 2;
+                case X -> 6;
+                case J -> 3;
+                case Q -> 4;
+                case K -> 5;
+                case A -> 7;
+            };
+        }
+    }
+
+    public String toString() {
         return switch(this) {
             case VII -> "VII";
             case VIII -> "VIII";
