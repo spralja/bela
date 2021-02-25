de2ff7d7b6e87b399b836897381a3c73bd724856
spralja <spralja@ruc.dk>
2021-02-25 06:24:54 +0100

    Added tostring method to Rank enum

--- src/bela/Rank.java
+++ src/bela/Rank.java
@@ -9,4 +9,18 @@ public enum Rank {
     Q,
     K,
     A,
+    ;
+
+    public String toString(Suit trumpSuit) {
+        return switch(this) {
+            case VII -> "VII";
+            case VIII -> "VIII";
+            case IX -> "IX";
+            case X -> "X";
+            case J -> "J";
+            case Q -> "Q";
+            case K -> "K";
+            case A -> "A";
+        };
+    }
 }
