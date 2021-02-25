9e4622aed1ff82966296ff0747f51fb9edd8d84c
spralja <spralja@ruc.dk>
2021-02-25 09:28:03 +0100

    Set up PlayerQuartet

--- src/bela/PlayerQuartet.java
+++ src/bela/PlayerQuartet.java
@@ -1,4 +1,13 @@
 package bela;
 
-public class PlayerQuartet extends PlayerCollection{
+public class PlayerQuartet extends PlayerCollection {
+    private static final int NUMBER_OF_PLAYERS = 4;
+    @Override
+    public boolean add(Player player) {
+        if(size() < NUMBER_OF_PLAYERS) {
+            return players.add(player);
+        } else {
+            return false;
+        }
+    }
 }
