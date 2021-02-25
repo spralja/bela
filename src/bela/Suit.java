434ab99152507d1b82af511ad37c0de8906dde68
spralja <spralja@ruc.dk>
2021-02-25 06:21:07 +0100

    Added toString(Suit trumpSuit) method to Suit enum

--- src/bela/Suit.java
+++ src/bela/Suit.java
@@ -5,4 +5,23 @@ public enum Suit {
     HEARTS,
     DIAMONDS,
     CLUBS,
+    ;
+
+    public String toString(Suit trumpSuit) {
+        if(this == trumpSuit) {
+            return switch(this) {
+                case SPADES -> "♠";
+                case HEARTS -> "♥";
+                case DIAMONDS -> "♦";
+                case CLUBS -> "♣";
+            };
+        } else {
+            return switch(this) {
+                case SPADES -> "♤";
+                case HEARTS -> "♡";
+                case DIAMONDS -> "♢";
+                case CLUBS -> "♧";
+            };
+        }
+    }
 }
