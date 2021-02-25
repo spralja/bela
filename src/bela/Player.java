d1e0145c84a15fed8af46ad63102573d54177ce4
spralja <spralja@ruc.dk>
2021-02-25 07:05:45 +0100

    Created Player superClass

---
+++ src/bela/Player.java
@@ -0,0 +1,16 @@
+/*
+package bela;
+
+public abstract class Player {
+    private Hand hand;
+
+    public abstract Card play(Game game);
+
+    public abstract Declaration declareDeclarations(Game game);
+
+    public abstract Suit declareTrumpSuit(Game game);
+
+    public abstract Player draw(Card card);
+}
+
+ */
